3580109dc36104305692ed12bc89d0215e93162c
pramodky15 <pkyntl0@gmail.com>
2023-01-29 02:12:49 -0800

    test1.py updatedcode

--- test1.py
+++ test1.py
@@ -1,3 +1,5 @@
+updting th code to understnd the staging.
 This is python project and it will be used for pythone project
 This is to check the how to commit after update.
 Live the life in this beutiful world.
+
